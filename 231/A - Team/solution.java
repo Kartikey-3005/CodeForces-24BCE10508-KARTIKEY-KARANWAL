865dd39af355cd87f58ebb77cd03d8cee9ac2cf8
Kartikey Karanwal <kartikeykaranwal03@gmail.com>
2026-08-05 10:27:42 +0530

    Add Team [A] from Codeforces

---
+++ 231/A - Team/solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+            
+            if (a + b + c >= 2) {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+        
+        scanner.close();
+    }
+}
