9375feae476ca90f80ddcbdb3095d817a6e94cce
Kartikey Karanwal <kartikeykaranwal03@gmail.com>
2026-08-24 02:04:45 +0530

    Add Divisibility Problem [A] from Codeforces

---
+++ 1328/A - Divisibility Problem/solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        
+        for (int i = 0; i < t; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            
+            if (a % b == 0) {
+                System.out.println(0);
+            } else {
+                System.out.println(b - (a % b));
+            }
+        }
+        
+        scanner.close();
+    }
+}
