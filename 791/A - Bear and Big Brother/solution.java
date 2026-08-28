5ac5608e977ee2c91b6b69ff1a30863b9f990a37
Kartikey Karanwal <kartikeykaranwal03@gmail.com>
2026-08-29 00:19:05 +0530

    Add Bear and Big Brother [A] from Codeforces

---
+++ 791/A - Bear and Big Brother/solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        
+        int years = 0;
+        
+        while (a <= b) {
+            a = a * 3;
+            b = b * 2;
+            years++;
+        }
+        
+        System.out.println(years);
+        
+        scanner.close();
+    }
+}
