88b4f93d0efa8daf354f24a018ea0b23a262203e
Kartikey Karanwal <kartikeykaranwal03@gmail.com>
2026-09-04 00:08:36 +0530

    Add Hulk [A] from Codeforces

---
+++ 705/A - Hulk/solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.print("I hate");
+            } else {
+                System.out.print("I love");
+            }
+            
+            if (i == n) {
+                System.out.print(" it");
+            } else {
+                System.out.print(" that ");
+            }
+        }
+        
+        System.out.println();
+        
+        scanner.close();
+    }
+}
