0cd3aeb1f5e46244ee6cc994176c36c1295380b0
Kartikey Karanwal <kartikeykaranwal03@gmail.com>
2026-09-04 20:25:54 +0530

    Add Drinks [B] from Codeforces

---
+++ 200/B - Drinks/solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        double sum = 0;
+        
+        for (int i = 0; i < n; i++) {
+            sum += scanner.nextInt();
+        }
+        
+        System.out.println(sum / n);
+        
+        scanner.close();
+    }
+}
